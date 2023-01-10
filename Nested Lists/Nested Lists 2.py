839b8173bd8af568ce1b5e2d1ef732257d48189b
Negar Loloshahvar <negarshahvar@gmail.com>
2023-01-10 15:35:30 +0000

    update

--- Nested Lists/Nested Lists 2.py
+++
@@ -1,9 +0,0 @@
-records = []
-for _ in range(int(input())):
-    name = input()
-    score = float(input())
-    records.append([name, score])
-
-records.sort(key= lambda item: (item[1], item[0]))
-print(records)
-
