308c5c2b2739784652b17d70e64b9ff3822494e5
Negar Loloshahvar <negarshahvar@gmail.com>
2023-01-11 15:03:00 +0000

    Update Nested Lists.py

--- 1 - Nested Lists/Nested Lists.py
+++ 1 - Nested Lists/Nested Lists.py
@@ -1,46 +1,25 @@
-# Given the names and grades for each student in a class of N students, store them in a nested list and print the name(s) of any student(s) having the second lowest grade.
-# Note: If there are multiple students with the second lowest grade, order their names alphabetically and print each name on a new line.
+records = []
 
-# if __name__ == '__main__':
-#     mydic = {}
-#     for _ in range(int(input())):
-#         name = input()
-#         score = float(input())
-#         mydic[name] = score
-#
-# mydic_sorted = sorted(mydic.items(), key= lambda x: x[1])
-# lowest = next(iter(mydic_sorted))
-# lowest_key = mydic_sorted.pop(lowest)
-#
-# print(mydic_sorted)
+for i in range(int(input('N= '))):
+    name = input('Name= ')
+    score = int(input('Score= '))
+    records.append([name, score])
 
-# Solution:
+records.sort(key= lambda item: (item[1], item[0]))
 
-# if __name__ == '__main__':
-#     dict_a = {}
-#     for _ in range(int(input())):
-#         name = input()
-#         score = float(input())
-#         dict_a[name] = score
-#     values=sorted(list(set(dict_a.values())))
-#     lst = []
-#     for key, value in dict_a.items():
-#         if values[1] == value:
-#             lst.append(key)
-#     lst.sort()
-#     for items in lst:
-#         print(items)
+print(records)
 
-# dict_a = {}
-# for _ in range(int(input())):
-#         name = input()
-#         score = float(input())
-#         dict_a[name] = score
-# values=sorted(list(set(dict_a.values())))
-# lst = []
-# for key, value in dict_a.items():
-#         if values[1] == value:
-#             lst.append(key)
-# lst.sort()
-# for items in lst:
-#         print(items)
+lowest = second_lowest = records[0][1]
+value_update = 0
+i = 0
+size = len(records)
+
+while i < size:
+    if records[i][1] > second_lowest:
+        second_lowest = records[i][1]
+        value_update += 1
+    if value_update == 1:
+        print(records[i][0])
+    if lowest != second_lowest and value_update > 2:
+        break
+    i += 1
